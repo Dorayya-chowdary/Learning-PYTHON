f3d9dad88a53584da4357b98b3e6718114540444
Dorayya Chowdary <80533487+Dorayya-chowdary@users.noreply.github.com>
2021-06-03 15:29:33 +0530

    Add files via upload

---
+++ Multiple_Range.py
@@ -0,0 +1,9 @@
+num=int(input('Enter the table:'))
+s=int(input('enter the start range of steps:' ))
+e=int(input('enter the end range of steps:'))
+multiple_s=int(input('enter the multiple range start:'))
+multiple_e=int(input('enter the multiple range end:'))
+for i in range(s,e+1,1):
+    if num*i>=multiple_s and num*i<=multiple_e:
+        print(num,'X',i,'=',num*i)
+
