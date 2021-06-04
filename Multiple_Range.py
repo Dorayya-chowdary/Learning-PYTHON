a226676240e9f8ddd5a88bce54d47f4b03f0dcfc
Dorayya Chowdary <80533487+Dorayya-chowdary@users.noreply.github.com>
2021-06-05 00:44:32 +0530

    Update Multiple_Range.py

--- Multiple_Range.py
+++ Multiple_Range.py
@@ -3,7 +3,7 @@ s=int(input('enter the start range of steps:' ))
 e=int(input('enter the end range of steps:'))
 multiple_s=int(input('enter the multiple range start:'))
 multiple_e=int(input('enter the multiple range end:'))
-for i in range(s,e+1,1)
+for i in range(s,e+1,1):
     if num*i>=multiple_s and num*i<=multiple_e:
         print(num,'X',i,'=',num*i)
 
