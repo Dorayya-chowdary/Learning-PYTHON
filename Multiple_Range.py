ee3b56f3bfcb7b6586fa3f1184804c8c41eeac32
Dorayya Chowdary <80533487+Dorayya-chowdary@users.noreply.github.com>
2021-06-29 21:31:12 +0530

    Update Multiple_Range.py

--- Multiple_Range.py
+++ Multiple_Range.py
@@ -5,5 +5,5 @@ multiple_s=int(input('enter the multiple range start:'))
 multiple_e=int(input('enter the multiple range end:'))
 for i in range(s,e+1,1):
     if num*i>=multiple_s and num*i<=multiple_e:
-        print(num,'X',i,'=',num*i
+        print(num,'X',i,'=',num*i)
 
